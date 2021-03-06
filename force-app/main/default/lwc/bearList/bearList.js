32b315c7c49fc1bee4a42c8f7007d760262272f2
Charles Ross <chivalry@mac.com>
2021-03-06 12:10:23 -0800

    refactor

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,25 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
-import getAllBears from '@salesforce/apex/BearController.getAllBears';
+import searchBears from '@salesforce/apex/BearController.searchBears';
 
 export default class BearList extends LightningElement {
-    bears;
-    error;
+	searchTerm = '';
+	@wire(searchBears, {searchTerm: '$searchTerm'}) bears;
     appResources = {
         bearSilhouette: `${ursusResources}/img/standing-bear-silhouette.png`
     };
 
-    connectedCallback() {
-        this.loadBears();
+	handleSearchTermChange(event) {
+		window.clearTimeout(this.delayTimeout);
+		const searchTerm = event.target.value;
+		this.delayTimeout = setTimeout(() => {
+			this.searchTerm = searchTerm;
+		}, 300);
 	}
 
-	loadBears() {
-		getAllBears()
-			.then(result => {
-				this.bears = result;
-			})
-			.catch(error => {
-				this.error = error;
-			});
+	get hasResults() {
+		return (this.bears.data.length > 0);
 	}
 }
